9efe1af342d5899db82423bd9fadd74a192ab6a9
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-10 23:10:58 +0200

    add sub

--- src/components/partials/Sub.vue
+++ src/components/partials/Sub.vue
@@ -15,8 +15,13 @@
   <section>
     <div class="container">
       <div id="sub">
-        <h3>subscribe</h3>
-        <input type="text">
+        <h3>Subscribe Now to Our Newsletter !</h3>
+        <div class="ds-search-bar">
+          <input type="text" placeholder="Email address...">
+          <div class="ds-circle ds-glass">
+            <i class="fa-regular fa-paper-plane"></i>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -31,14 +36,39 @@
 
 <style lang="scss" scoped>
   @use '../../scss/partials/mixin' as *;
+  @use '../../scss/partials/var' as *;
 
   section {
-    border: 1px solid black;
+    padding: 55px;
+    color: $text-1;
+    background-color: $background-6;
 
     #sub {
       @include flex('vertical');
       justify-content: space-around;
-      border: 1px solid black;
+
+      .ds-search-bar {
+        display: flex;
+        justify-content: space-between;
+        width: 470px;
+        height: 75px;
+        padding-left: 25px;
+        background-color: $background-1;
+        border-radius: 50px;
+
+      input {
+        border: none;
+        border-radius: 50px;
+        color: $text-6;
+      }
+  
+      .ds-circle.ds-glass {
+        margin-right: 0;
+        width: 75px;
+        background-color: $background-5;
+      }
+    
+    }
     }
   }
 </style>
