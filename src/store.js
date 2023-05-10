587a760d04ee316da398630b1cad1f60392c73c4
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-10 10:48:17 +0200

    add headerMenu, dropMenu

--- src/store.js
+++ src/store.js
@@ -1,4 +1,39 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
+  headerMenu: [
+    'Home',
+    'Courses',
+    'Abouts Us',
+    'News',
+    'Pages',
+    'Contact',
+    'Purchase'
+  ],
+
+  dropMenu: {
+    home: [
+      'Driving School',
+      'High School',
+      'Kinder Garten',
+      'Header Video'
+    ],
+    courses: [
+      'Our Courses',
+      'Single Course',
+      'Instructor Profile',
+      'Become A Teacher'
+    ],
+    news: [
+      'Edu Prime Blog',
+      'Single Post',
+      'Category Posts'
+    ],
+    pages: [
+      'How It Works',
+      'Single Event',
+      'University Shop',
+      'Single Product'
+    ]
+  }
 }) 
