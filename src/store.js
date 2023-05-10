5225c4b1f51c5cfb0873132da8246b51e21ac654
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-10 11:02:11 +0200

    add navMenu

--- src/store.js
+++ src/store.js
@@ -1,39 +1,50 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
-  headerMenu: [
-    'Home',
-    'Courses',
-    'Abouts Us',
-    'News',
-    'Pages',
-    'Contact',
-    'Purchase'
-  ],
-
   dropMenu: {
-    home: [
-      'Driving School',
-      'High School',
-      'Kinder Garten',
-      'Header Video'
-    ],
-    courses: [
-      'Our Courses',
-      'Single Course',
-      'Instructor Profile',
-      'Become A Teacher'
-    ],
-    news: [
-      'Edu Prime Blog',
-      'Single Post',
-      'Category Posts'
-    ],
-    pages: [
-      'How It Works',
-      'Single Event',
-      'University Shop',
-      'Single Product'
-    ]
+    li: {
+      name: 'Home',
+      menu: [
+        'Driving School',
+        'High School',
+        'Kinder Garten',
+        'Header Video'
+      ]
+    },
+    li: {
+      name: 'Courses',
+      menu: [
+        'Our Courses',
+        'Single Course',
+        'Instructor Profile',
+        'Become A Teacher'
+      ]
+    },
+    li: {
+      name: 'Abouts Us'
+    },
+    li: {
+      name: 'News',
+      menu: [
+        'Edu Prime Blog',
+        'Single Post',
+        'Category Posts'
+      ]
+    },
+    li: {
+      name: 'Pages',
+      menu: [
+        'How It Works',
+        'Single Event',
+        'University Shop',
+        'Single Product'
+      ]
+    },
+    li: {
+      name: 'Contact',
+    },
+    li: {
+      name: 'Purchase',
+    }
   }
 }) 
